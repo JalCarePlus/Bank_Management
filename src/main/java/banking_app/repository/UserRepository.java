010e5fac2282ac41dcc3cf4501bc98230a8738de
Jalcare Project <suyogkulkarni2004@gmail.com>
2026-02-25 22:55:13 +0530

    Delete src/main directory

--- src/main/java/banking_app/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package banking_app.repository;
-
-import banking_app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
