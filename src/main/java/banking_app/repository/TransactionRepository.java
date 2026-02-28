f2f0a40d117152ea80e1624f2d79458c48ff2e6a
Jalcare Project <suyogkulkarni2004@gmail.com>
2026-03-01 00:12:03 +0530

    Update TransactionRepository.java

--- src/main/java/banking_app/repository/TransactionRepository.java
+++ src/main/java/banking_app/repository/TransactionRepository.java
@@ -13,6 +13,8 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByUser(User user);
     
+    List<Transaction> findTop10ByOrderByDateTimeDesc();
+    
     @Query("SELECT FUNCTION('DATE', t.dateTime), COUNT(t) FROM Transaction t WHERE t.dateTime >= :since GROUP BY FUNCTION('DATE', t.dateTime)")
     List<Object[]> getDailyTransactionCount(@Param("since") LocalDateTime since);
     
