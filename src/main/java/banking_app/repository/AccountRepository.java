4f12d512e8b96caf3f4fecf152c5b1cf5b63474a
Jalcare Project <suyogkulkarni2004@gmail.com>
2026-02-28 18:56:37 +0530

    Refactor AccountRepository with repository annotation
    Added @Repository annotation and rearranged methods.

--- src/main/java/banking_app/repository/AccountRepository.java
+++ src/main/java/banking_app/repository/AccountRepository.java
@@ -3,18 +3,10 @@ package banking_app.repository;
 import banking_app.entity.Account;
 import banking_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByAccountNumber(String accountNumber);
     Account findByUser(User user);
-    
-    // Optional methods
-    List<Account> findByBalanceLessThan(Double threshold);
-    
-    @Query("SELECT SUM(a.balance) FROM Account a")
-    Double getTotalBalance();
-    
-    long countByBalanceGreaterThan(Double threshold);
+    Account findByAccountNumber(String accountNumber);
 }
